157e93e4a9110398bcbaca57039232d251389a41
WoH3D <j200chi@naver.com>
2025-11-04 15:33:19 +0900

    파일 위치 변경, dashboard flask 연동, dashboard 색상 변경

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder='frontend')
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
